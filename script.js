ec417e9e1abbd9df23ee8d8eba430a3ffa377919
DidacDV <113896749+DidacDV@users.noreply.github.com>
2024-06-15 00:07:46 +0200

    added UI with js interactions

--- script.js
+++ script.js
@@ -8,6 +8,37 @@ let humanWins = 0;
 let computerWins = 0;
 let winner; //if 0, human, if 1 , computer
 
+const currentScore = document.querySelector("#points");
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const hscore = document.querySelector("#hscore");
+const cscore = document.querySelector("#cscore");
+
+function incrhscore() {
+    let current = parseInt(hscore.textContent);
+    current++;
+    hscore.textContent = current.toString();
+}
+
+function incrcscore() {
+    let current = parseInt(cscore.textContent);
+    current++;
+    cscore.textContent = current.toString();
+}
+
+rockButton.addEventListener("click",function() {
+    playround(0,getComputerChoice());
+})
+
+paperButton.addEventListener("click",function() {
+    playround(1,getComputerChoice());
+})
+
+scissorsButton.addEventListener("click",function() {
+    playround(2,getComputerChoice());
+})
+
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
@@ -26,46 +57,66 @@ function playround(humanChoice,computerChoice) {
         if (humanChoice == 0) {
             if (computerChoice == 1) {
                 ++computerWins; //paper beats rock
+                incrcscore();
                 console.log("Computer won");    
             }
             else if (computerChoice == 2) {
                 ++humanWins; //rock beats scissor
+                incrhscore();
                 console.log("You won");   
             }
         }
         else if(humanChoice == 1) {
             if (computerChoice == 0) {
                 ++humanWins;
+                incrhscore();
                 console.log("You won");
             } 
             else if (computerChoice == 2) {
                 ++computerWins;
+                incrcscore();
                 console.log("Computer won");
             }
         }
         else if (humanChoice == 2) {
             if (computerChoice == 0) {
                 ++computerWins;
+                incrcscore();
                 console.log("Computer won");
             }
             else if (computerChoice == 1) {
                 ++humanWins;
+                incrhscore();
                 console.log("You won");
             }
         }
     }
     console.log(`Score: Computer:" ${computerWins} - ${humanWins} :Human`);   
+    checkAndUpdate();
 }
 
-function play() {
-    for (let i = 0; i < 5; i++) {
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
-        playround(humanChoice,computerChoice);
+function checkAndUpdate() {
+    let bc = computerWins === 5;
+    let bh = humanWins === 5; 
+    if (bc || bh) {
+        var text =  document.createElement("label");
+        var div = document.createElement("div");
+        div.id = "winnerd";
+        div.appendChild(text)
+
+        currentScore.appendChild(div);
+        computerWins = humanWins = 0;
+        hscore.textContent = "0";
+        cscore.textContent = "0";
+        if (bc) {
+            text.textContent = "Computer Won :(";
+        }
+        else if (bh) {
+            text.textContent = "You Won :D";
+        }
+        setTimeout(function(){
+            currentScore.removeChild(div);
+        },1700);
     }
-    winner = computerWins > humanWins;
-    if (winner = "computer") console.log("Computer won :(");
-    else console.log("You won!");
 }
 
-play();
